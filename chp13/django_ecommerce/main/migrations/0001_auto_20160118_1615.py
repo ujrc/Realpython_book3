bd398306cd659376440a48badb88e78325e10f51
ujrc <uwjearc@yahoo.com>
2016-01-19 10:38:47 -0600

    updates migrations

---
+++ chp13/django_ecommerce/main/migrations/0001_auto_20160118_1615.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', 'data_load_marketing_items_0005'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='announcement',
+            name='publication_date',
+            field=models.DateTimeField(verbose_name=datetime.datetime(2016, 1, 18, 22, 15, 19, 138018, tzinfo=utc)),
+        ),
+    ]
